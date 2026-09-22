d0f5eb7373cfdb1a8d256e9f48b4c10abfeb9a15
babludangi6266 <babludangi2000@gmail.com>
2026-09-22 15:32:24 +0530

    feat: add axios api service and update proxy target to production endpoint

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     port: 5175,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5000',
+        target: 'https://zaminjunction.com/api/',
         changeOrigin: true,
         secure: false,
       },
